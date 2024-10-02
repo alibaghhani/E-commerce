3538e647477200d3e025a5f110715cd73a03394e
alibaghani <baghaniali2006@gmail.com>
2024-10-02 11:26:03 +0330

    feat(auth): create user model and manager
    create user model and manager for managing users and superusers

---
+++ authentication/managers.py
@@ -0,0 +1,42 @@
+from django.contrib.auth.models import BaseUserManager
+
+
+class UserManager(BaseUserManager):
+    def create_user(self, email=None, username=None, password=None, **extra_fields):
+        """
+        Create and save a regular User with the given email, username, and password.
+        """
+        if not email and not username:
+            raise ValueError('Either email or username must be set')
+
+        if email:
+            email = self.normalize_email(email)
+
+        user = self.model(
+            email=email,
+            username=username,
+            **extra_fields
+        )
+
+        if email:
+            user.email = email
+        if username:
+            user.username = username
+
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, username=None, password=None, **extra_fields):
+        """
+        create superuser
+        """
+        extra_fields.setdefault('is_superuser', True)
+        extra_fields.setdefault('is_staff', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('Superuser must have is_superuser=True.')
+
+        return self.create_user(email, username, password, **extra_fields)
--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.models import AbstractUser
+from django.core.validators import RegexValidator
 from django.db import models
+from core.models import TimeStampMixin
+from .managers import UserManager
 
-# Create your models here.
+
+class User(AbstractUser):
+    """
+    User model with additional fields and custom validations.
+    Attributes:
+        email (EmailField): User's email address.
+        username (CharField): User's username.
+        is_superuser (BooleanField): Indicates whether the user is a superuser.
+        is_staff (BooleanField): Indicates whether the user is a staff member.
+        password (CharField): User's password.
+
+    """
+    email_validator = RegexValidator(
+        regex=r"^[a-zA-Z0-9._%+-]+@gmail\.com$",
+        message='لطفا یک ایمیل درست وارد کنید'
+    )
+    username_validator = RegexValidator(
+        regex=r'^.{8,}$',
+        message='Username must be at least 8 characters long and contain at least one uppercase letter.',
+    )
+    email = models.EmailField(
+        verbose_name='email address',
+        max_length=255,
+        unique=True,
+        validators=[email_validator]
+    )
+
+    username = models.CharField(
+        verbose_name='username',
+        max_length=30,
+        unique=True,
+        validators=[username_validator])
+
+    is_superuser = models.BooleanField(default=False)
+    is_staff = models.BooleanField(default=False)
+    objects = UserManager()
+
+    REQUIRED_FIELDS = ['email']
+
+    def __str__(self):
+        return f'{self.username}---{self.email}'
+
+    def save(self, *args, **kwargs):
+        if self.is_superuser == False:
+            if not self.pk:
+                self.set_password(self.password)
+        super().save(*args, **kwargs)
