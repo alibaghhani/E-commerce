94ba04a86b80bcc4d450e0b118ec9b0f7fafd545
alibaghani <baghaniali2006@gmail.com>
2024-10-02 11:13:19 +0330

    feat(mixin model): create mixin model
    mixin model for created_at, updated_at, expired_at

--- core/models.py
+++ core/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+
+class TimeStampMixin(models.Model):
+    """
+    A base model with fields for creation, update, and expiration timestamps.
+    Attributes:
+        created_at (DateTimeField): Timestamp for creation.
+        updated_at (DateTimeField): Timestamp for last update.
+        expired_at (DateTimeField): Timestamp for expiration.
+    """
+
+    created_at = models.DateTimeField(auto_now_add=True, editable=False)
+    updated_at = models.DateTimeField(auto_now=True, editable=False)
+    expired_at = models.DateTimeField(auto_now=True, editable=False)
+
+    class Meta:
+        abstract = True
+
