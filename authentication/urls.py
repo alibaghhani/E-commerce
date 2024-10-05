12e11525a313d4d9639db0e1434ac4e826dd5692
alibaghani <baghaniali2006@gmail.com>
2024-10-05 17:54:09 +0330

    feat(endpoint): add userprofile and addresses routers

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,8 +1,10 @@
-from django.urls import path
-from rest_framework.routers import DefaultRouter
-from .views import CustomerViewSet, SellerViewSet
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter, SimpleRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
+from rest_framework_nested import routers
+from .views import CustomerViewSet, SellerViewSet, UsersProfileViewSet, UserAddressesViewSet
 
-router = DefaultRouter()
+router = SimpleRouter()
 router.register(
     r'customer/register',
     CustomerViewSet,
@@ -15,6 +17,30 @@ router.register(
     basename='seller'
 )
 
-urlpatterns = []
+router.register(
+    r'users',
+    UsersProfileViewSet,
+    basename='customer-profile'
+)
 
-urlpatterns += router.urls
+address_router = routers.NestedSimpleRouter(
+    router,
+    r'users',
+    lookup='user'
+)
+
+address_router.register(
+    r'addresses',
+    UserAddressesViewSet,
+    basename='user-addresses'
+)
+
+urlpatterns = [
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+    path('', include(router.urls)),
+    path('', include(address_router.urls))
+    ]
+
+
