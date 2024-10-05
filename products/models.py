4441774da8a11545796ebf2a8fc95db12f2ef822
alibaghani <baghaniali2006@gmail.com>
2024-10-05 17:57:13 +0330

    feat(database): create product model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,25 @@
+from django.core.validators import FileExtensionValidator
 from django.db import models
+from core.models import TimeStampMixin
 
-# Create your models here.
+
+class Product(TimeStampMixin):
+    """
+    Product model for save products.
+    Attributes:
+        name (CharField): Product name.
+        price (PositiveIntegerField): Product price.
+        detail (TextField): Product description.
+        category (ForeignKey): Foreign key to Category model.
+        warehouse (PositiveIntegerField): Product warehouse number.
+        slug (SlugField): Product slug.
+    """
+    name = models.CharField(max_length=250)
+    price = models.PositiveIntegerField()
+    detail = models.TextField(max_length=250)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, related_name='product_category')
+    warehouse = models.PositiveIntegerField(null=True, blank=True)
+    slug = models.SlugField(unique=True)
+
+    expired_at = None
+
