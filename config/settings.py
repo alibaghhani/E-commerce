25d6dda08ce98ce6b2ceec19589016985743da6f
alibaghani <baghaniali2006@gmail.com>
2024-10-05 17:58:57 +0330

    feat(auth): add jwt settings

--- config/settings.py
+++ config/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'rest_framework',
     'debug_toolbar',
+    'rest_framework_simplejwt',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
