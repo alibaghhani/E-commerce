83db53b7745669d72fc1f6be882aaa4cffc9c1f8
alibaghani <baghaniali2006@gmail.com>
2024-10-06 14:06:12 +0330

    test: change user_data in setup method

--- authentication/tests/tests_authentication_models.py
+++ authentication/tests/tests_authentication_models.py
@@ -1,7 +1,8 @@
+import uuid
 from django.contrib.auth.hashers import make_password
 from django.test import TestCase
 
-from authentication.models import User, SellerProfile, CustomerProfile
+from authentication.models import User, SellerProfile, CustomerProfile, Address
 
 
 # Create your tests here.
@@ -21,7 +22,8 @@ class UsersAndProfilesTestCase(TestCase):
             "password": "test12345678",
             "is_seller": True,
             "is_superuser": False,
-            "is_staff": False
+            "is_staff": False,
+            "uuid": uuid.uuid4()
         }
         self.customer_data = {
             "email": "testcustomer@gmail.com",
@@ -29,7 +31,8 @@ class UsersAndProfilesTestCase(TestCase):
             "password": "test12345678",
             "is_seller": False,
             "is_superuser": False,
-            "is_staff": False
+            "is_staff": False,
+            "uuid": uuid.uuid4()
         }
         self.seller = User.objects.create(**self.seller_data)
         self.customer_user = User.objects.create(**self.customer_data)
@@ -59,3 +62,33 @@ class UsersAndProfilesTestCase(TestCase):
         Test password hashing
         """
         self.assertNotEqual(self.customer_data['password'], self.customer_user.password)
+
+
+class AddressTestCase(TestCase):
+    def setUp(self):
+        self.user_data = {
+            "username": "test_username",
+            "password": "test_password",
+            "email": "test_email",
+            "is_seller": False,
+            "is_superuser": False,
+            "is_staff": False
+        }
+        self.user = User.objects.create(**self.user_data)
+
+        self.address_data = {
+            "costumer": self.user,
+            "province": "Tehran",
+            "city": "Tehran",
+            "street": "1st Golbarg",
+            "house_number": 5,
+            "full_address": "1st Golbarg, Narenjestan Blvd., Shams Abaad Ind.Town, Hasan Abaad Old Rd."
+
+        }
+
+    def test_create_address_object(self):
+        Address.objects.create(**self.address_data)
+        address = Address.objects.get(costumer__id=self.user.id)
+        self.assertEqual(address.province, self.address_data['province'])
+
+
