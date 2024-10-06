a9d140655b23a60e920946e1c39d1943d5ec2e6f
alibaghani <baghaniali2006@gmail.com>
2024-10-06 17:20:42 +0330

    test(test): test is error raises or not

--- authentication/tests/test_authentication_endpoints.py
+++ authentication/tests/test_authentication_endpoints.py
@@ -1,3 +1,4 @@
+from django.db import IntegrityError
 from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
@@ -23,10 +24,15 @@ class CustomerProfileTestCase(APITestCase):
 
     def test_create_costumer_profile(self):
         self.user_data.update(self.customer_profile_data)
-        print(self.user_data)
         response = self.client.post(self.base_url, self.user_data, format='json')
-        print(dir(response))
-        print(response.json)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_if_IntegrityError_raises_properly(self):
+        self.user_data.update(self.customer_profile_data)
+        self.client.post(self.base_url, self.user_data, format='json')
+        response = self.client.post(self.base_url, self.user_data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
+        self.assertNotEqual(response.status_code, status.HTTP_201_CREATED)
+
 
 
