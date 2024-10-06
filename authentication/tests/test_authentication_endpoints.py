55924a339502195eb36d9584a311be6efbef6ebc
alibaghani <baghaniali2006@gmail.com>
2024-10-06 15:27:59 +0330

    test(test): test create customer profile

--- authentication/tests/test_authentication_endpoints.py
+++ authentication/tests/test_authentication_endpoints.py
@@ -1,4 +1,7 @@
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase, APIClient
+from collections import ChainMap
 
 
 class CustomerProfileTestCase(APITestCase):
@@ -6,9 +9,24 @@ class CustomerProfileTestCase(APITestCase):
         self.client = APIClient()
 
         self.user_data = {
-            "username": "test_user",
-            "password": "test_password",
-            "email": "test_email@gmail.com"
+            "email": "test_email@gmail.com",
+            "username": "test_userJGJGJ",
+            "password": "test_passwordGFG%^%&h",
+
+        }
+        self.customer_profile_data = {
+            "first_name": "John",
+            "last_name": "Doe"
         }
 
+        self.base_url = reverse('customer-list')
+
     def test_create_costumer_profile(self):
+        self.user_data.update(self.customer_profile_data)
+        print(self.user_data)
+        response = self.client.post(self.base_url, self.user_data, format='json')
+        print(dir(response))
+        print(response.json)
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
