64d64d493bbfcd63b7bbd00cb6e3164f93a6d649
alibaghani <baghaniali2006@gmail.com>
2024-10-07 17:01:38 +0330

    fix(permission): fix bug isowner permisiion

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -3,6 +3,6 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.method in SAFE_METHODS:
+        if obj.id == request.user.id:
             return True
-        return obj.costumer == request.user
+        return False
