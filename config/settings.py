b92cd80935615e7b8d9ee8fac4b7ddfe044b3ac4
alibaghani <baghaniali2006@gmail.com>
2024-10-07 17:02:33 +0330

    refactor(jwt): increase jwt life time

--- config/settings.py
+++ config/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 import sys
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -129,3 +130,9 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 AUTH_USER_MODEL = ('authentication.User')
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=500),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=1),
+    "ROTATE_REFRESH_TOKENS": False,
+    "BLACKLIST_AFTER_ROTATION": False,
+    "UPDATE_LAST_LOGIN": False}
