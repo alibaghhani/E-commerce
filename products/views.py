ad9b4428f4c2cb01180b971404af2c37fa6dbd55
alibaghani <baghaniali2006@gmail.com>
2024-10-08 12:44:43 +0330

    feat: create category viewset
    create category viewset and override get_permissions , get_queryset and get serializer_class methods

--- products/views.py
+++ products/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+from rest_framework.viewsets import GenericViewSet
+from .models import Category, Product
+from .serializers import CategoryDetailActionSerializer, CategoryListActionSerializer
+from rest_framework.permissions import IsAdminUser, AllowAny
+from rest_framework.viewsets import ModelViewSet
+
 
 # Create your views here.
+
+class CategoryViewSet(ModelViewSet):
+    serializer_class = CategoryListActionSerializer
+    queryset = Category.objects.all()
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        is_seller = self.request.query_params.get('category')
+        if is_seller == 'parents':
+            queryset = queryset.filter(parent=None)
+        return queryset
+
+    def get_serializer_class(self, *args, **kwargs):
+        if self.action == 'list':
+            return CategoryListActionSerializer
+        if self.action == 'retrieve':
+            return CategoryDetailActionSerializer
+        else:
+            return self.serializer_class
+
+    def get_permissions(self):
+        if self.action == 'create':
+            return [IsAdminUser()]
+        return [AllowAny()]
