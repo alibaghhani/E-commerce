1aebfa64226d96f200eecfa9780954fade09c1d9
alibaghani <baghaniali2006@gmail.com>
2024-10-08 12:43:17 +0330

    feat(serializer): create category serializers
    implement different serializer for each action

---
+++ products/serializers.py
@@ -0,0 +1,26 @@
+from authentication.models import User
+from .models import Product, Category, Image
+from rest_framework import serializers
+
+
+class CategoryListActionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Category
+        fields = ('id', 'name', 'parent')
+
+
+class CategoryDetailActionSerializer(serializers.ModelSerializer):
+    product_amount = serializers.SerializerMethodField()
+    parent_name = serializers.SerializerMethodField()
+
+    class Meta:
+        model = Category
+        fields = ('name', 'parent', 'product_amount', 'parent_name')
+
+    def get_product_amount(self, obj):
+        return obj.product_category.count()
+
+    def get_parent_name(self, obj):
+        if obj.parent:
+            return obj.parent.name
+        return None
