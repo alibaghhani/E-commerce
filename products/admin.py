b3e066ac7100523b3372e5811840d629448cc685
alibaghani <baghaniali2006@gmail.com>
2024-10-08 12:41:10 +0330

    feat: add models to admin pannel

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import (Product, Category, Image)
+from authentication.models import (User, SellerProfile, CustomerProfile, Address)
+
+admin.site.register(Product)
+admin.site.register(Category)
+admin.site.register(Image)
+admin.site.register(User)
+admin.site.register(SellerProfile)
+admin.site.register(CustomerProfile)
+admin.site.register(Address)
