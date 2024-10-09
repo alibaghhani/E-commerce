3c8b96c0fc80c0f0b1d95e00bb95e5e8585ae5fe
alibaghani <baghaniali2006@gmail.com>
2024-10-09 15:07:22 +0330

    test: create test for product model's view

---
+++ products/tests/test_product_endpoints.py
@@ -0,0 +1,28 @@
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APIClient, APITestCase
+from authentication.models import User, SellerProfile, CustomerProfile
+from authentication.seralizers import UserSerializer
+from products.models import Category
+
+
+class ProductAndCategoryTestCase(APITestCase):
+    def setUp(self):
+        self.client = APIClient()
+        self.user_data = {
+            "username": "TestUsername",
+            "email": "TestEmail@gmail.com",
+            "password": "TestUsernameP@ssword",
+        }
+        self.category_base_url = reverse('categories-list')
+
+    def test_admin_can_see_list_of_products(self):
+        admin = User.objects.create_superuser(**self.user_data)
+        self.client.force_authenticate(user=admin)
+        response = self.client.get(self.category_base_url)
+        queryset = Category.objects.all()
+        serializer = UserSerializer(queryset, many=True)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data, serializer.data)
+
+
