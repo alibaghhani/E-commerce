b4e0d9d2a42448a6d757b6a92c071ab63bbf2a2b
alibaghani <baghaniali2006@gmail.com>
2024-10-09 10:42:38 +0330

    feat(auth): change Iswoner permission functionality

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -5,13 +5,13 @@ class IsOwner(BasePermission):
     def has_object_permission(self, request, view, obj):
         if obj.id == request.user.id:
             return True
+        if request.user.is_superuser:
+            return True
         return False
 
 
 class IsSellerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        if obj.seller == request.user.id:
-            return True
         if obj.seller == request.user.id:
             return True
         return False
