63beaff159c36b55a5dce9fcf34a32ff166f436a
alibaghani <baghaniali2006@gmail.com>
2024-10-09 13:02:12 +0330

    test: write test for product and category models

--- products/tests/test_product_model.py
+++ products/tests/test_product_model.py
@@ -1,3 +1,41 @@
 from django.test import TestCase
 
-# Create your tests here.
+from authentication.models import User, SellerProfile
+from products.models import Product, Category
+
+
+class ProductAdnCategoryTestCase(TestCase):
+    def setUp(self):
+        self.user = User.objects.create(
+            email='testemail@gmail.com',
+            username='testUsername()',
+            password='TestP@assword',
+            is_seller=True
+        )
+        self.seller = SellerProfile.objects.create(
+            user=self.user
+
+        )
+        self.category = Category.objects.create(
+            name='testcategory'
+        )
+        self.product_data = {
+            "name": "Sample Product",
+            "price": 1500,
+            "detail": "This is a sample product with detailed description.",
+            "category": self.category,
+            "warehouse": 100,
+            "slug": "sample-product",
+            "seller": self.seller
+        }
+
+    def test_create_product(self):
+        product = Product.objects.create(**self.product_data)
+        self.assertEqual(self.product_data['price'], product.price)
+        self.assertEqual(product.__str__(), product.name)
+
+    def test_create_category(self):
+        category = Category.objects.create(name='testcategory')
+        category.get_all_parents()
+        self.assertEqual(category.__str__(), category.name)
+        self.assertIn(category, category.get_all_parents())
