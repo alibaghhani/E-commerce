9502e69c1f3c03b166e7aacf6868a401b70f6bed
alibaghani <baghaniali2006@gmail.com>
2024-10-13 14:37:31 +0330

    fix: fix isseeler permission's bug

--- authentication/permissions.py
+++ authentication/permissions.py
@@ -10,8 +10,15 @@ class IsOwner(BasePermission):
         return False
 
 
-class IsSellerOrReadOnly(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        if obj.seller == request.user.id:
+class IsSellerOrAdminOrReadOnly(BasePermission):
+
+    def has_permission(self, request, view):
+        if request.method in SAFE_METHODS:
             return True
-        return False
+        return request.user.is_authenticated and request.user.is_seller or request.user.is_superuser
+
+    def has_object_permission(self, request, view, obj):
+        print('this method has been called has object permission')
+        if request.method in SAFE_METHODS:
+            return True
+        return obj.seller == request.user
