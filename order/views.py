0549a10bc8361000b5627c54e3b4762e79aaec61
alibaghani <baghaniali2006@gmail.com>
2024-10-14 16:37:47 +0330

    feat: implement view for Basket model

--- order/views.py
+++ order/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.authentication import JWTAuthentication
 
-# Create your views here.
+from .serializers import BasketCreateSerializer
+from rest_framework.viewsets import ModelViewSet
+from .models import Basket
+
+
+class BasketViewSet(ModelViewSet):
+    permission_classes = [AllowAny]
+    queryset = Basket.objects.all()
+    serializer_class = BasketCreateSerializer
+    authentication_classes = [JWTAuthentication]
+
+    def get_queryset(self, *args, **kwargs):
+        user = self.request.user
+        if user.is_authenticated:
+            return Basket.objects.filter(customer=user.customer_profile)
+        return Basket.objects.none()
+
+    def get_serializer_class(self):
+        if self.action in ['create', 'update']:
+            return BasketCreateSerializer
+        return self.serializer_class
