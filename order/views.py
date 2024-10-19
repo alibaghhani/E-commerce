27e49a8fcc646ec63b5c20172f338db0def9eaf7
alibaghani <baghaniali2006@gmail.com>
2024-10-19 10:10:39 -0400

    feat: change list method and remove prints

--- order/views.py
+++ order/views.py
@@ -12,6 +12,7 @@ import logging
 
 logger = logging.getLogger(__name__)
 
+
 class BasketViewSet(ModelViewSet):
     permission_classes = [IsAuthenticated]
     queryset = Basket.objects.all()
@@ -24,40 +25,37 @@ class BasketViewSet(ModelViewSet):
             return Basket.objects.filter(customer=user.customer_profile)
         return Basket.objects.none()
 
-    # def destroy(self, request, *args, **kwargs):
-    #     product_id = kwargs.get('product_id')
-    #     basket = Basket.objects.get(customer=request.user.id)
-    #     print(basket.products_list)
-
     def create(self, request, *args, **kwargs):
         serializer = BasketCreateSerializer(data=request.data, context={"request": request})
         if serializer.is_valid():
-            logger.debug("Received request data: %s", request.data)
             product_id = serializer.validated_data['product']
             product = Product.objects.get(id=product_id)
             quantity = serializer.validated_data['quantity']
             if product.warehouse >= quantity:
                 serializer.save()
-                print(f"=====entered id{product_id}")
-                print(f"=====entered quantity{quantity}")
-
-                print(f"======product warehouse{product.warehouse}")
-                print(f"======product name{product.name}")
-
-                print(f"{product.warehouse - quantity}")
                 Product.objects.filter(id=product_id).update(warehouse=product.warehouse - quantity)
             else:
-                logger.debug("Received request data: %s", request.data)
                 return Response(
                     {"message": "product's warehouse is lower than chosen quantity"},
                     status=status.HTTP_422_UNPROCESSABLE_ENTITY
                 )
-        logger.debug("Received request data: %s", request.data)
         return Response(serializer.errors)
 
     def list(self, request, *args, **kwargs):
         customer_id = request.user.customer_profile.id
         basket = Basket.objects.get(customer__id=customer_id)
-        product_ids = basket.products_list.value_to_list()
-        print(product_ids)
-        return Response({"status": f"{basket.products_list}"})
+        products_ids = [item['id'] for item in basket.products_list]
+        products_name = [Product.objects.get(id=i).name for i in products_ids]
+        print(products_name)
+        display_basket = {}
+        if products_name:
+            for i in products_name:
+                display_basket.update(
+                    {i: item['quantity'] for item in basket.products_list if
+                     item['id'] in [Product.objects.get(name=i).id]}
+                )
+                print(request.user.customer_profile.id)
+                print(display_basket)
+        else:
+            return Response({"message": "your basket is empty"})
+        return Response({"your basket": display_basket})
