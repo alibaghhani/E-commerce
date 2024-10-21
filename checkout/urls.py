c7ee44c91cda1340df5474b9bef08df1f3c48a6b
alibaghani <baghaniali2006@gmail.com>
2024-10-21 06:20:40 -0400

    refactor: delete basket and all files that related to basket
    I have changed my mind  and decided to implement basket with redis

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,14 +1,2 @@
-from rest_framework.routers import DefaultRouter
 
-from checkout.views import OrderCheckoutViewSet
 
-router = DefaultRouter()
-
-router.register(
-    r'submit_address',
-    OrderCheckoutViewSet,
-    'submit_order'
-)
-
-urlpatterns = []
-urlpatterns += router.urls
