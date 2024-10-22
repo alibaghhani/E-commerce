a5378136f78da8823b32000f271d20b1d250d035
alibaghani <baghaniali2006@gmail.com>
2024-10-22 03:54:46 -0400

    refactor: change django cache to redis

--- config/settings.py
+++ config/settings.py
@@ -161,7 +161,17 @@ SWAGGER_SETTINGS = {
 }
 
 redis_client = redis.StrictRedis(
-        host="localhost",
-        port=6379,
-        db=1,
+    host="localhost",
+    port=6379,
+    db=1,
 )
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/0",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "example"
+    }
+}
