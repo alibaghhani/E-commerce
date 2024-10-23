4644b94529f208225ac4ff928807e312f66b9143
alibaghani <baghaniali2006@gmail.com>
2024-10-23 04:42:35 -0400

    feat: add submit basket viewset's routers

--- order/urls.py
+++ order/urls.py
@@ -1,12 +1,24 @@
-from rest_framework.routers import DefaultRouter
-
-from order.views import BasketViewSet
-
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter,SimpleRouter
+from order.views import BasketViewSet,BasketSubmitViewSet
+from rest_framework_nested import routers
 router = DefaultRouter()
 router.register(
     r'basket',
     BasketViewSet,
     'basket'
 )
-urlpatterns = []
+
+
+router.register(
+    r'submit_basket',
+    BasketSubmitViewSet,
+    'submit_basket'
+
+)
+
+
+urlpatterns = [
+
+]
 urlpatterns += router.urls
