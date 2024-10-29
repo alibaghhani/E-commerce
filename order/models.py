65950b9d7681adaa96fb694115519ad47f0193e5
alibaghani <baghaniali2006@gmail.com>
2024-10-29 09:22:59 -0400

    feat: implement order model

--- order/models.py
+++ order/models.py
@@ -2,7 +2,25 @@ from django.contrib.auth import get_user_model
 
 from core.models import TimeStampMixin
 from django.db import models
+
+from products.models import Product
+
 User = get_user_model()
+
+class Order(TimeStampMixin):
+    user = models.ForeignKey(to=User, on_delete=models.CASCADE, related_name='user_order')
+    product_list = models.JSONField(default=dict)
+    address = models.TextField(default='iran')
+    total_price = models.FloatField(default='4.20')
+    expired_at = None
+
+    def __str__(self):
+        return f"{self.user} --- {self.address}"
+
+
+
+
+
 
 class DiscountCode(TimeStampMixin, models.Model):
     """
