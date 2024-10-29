cb7e42061a8b72987ad380664ccbc64f445b8519
alibaghani <baghaniali2006@gmail.com>
2024-10-29 09:25:04 -0400

    feat: set permission for payment view

--- checkout/views.py
+++ checkout/views.py
@@ -2,11 +2,12 @@ from rest_framework.response import Response
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from order.basket import BasketAndOrderRedisAdapter
 from django.http import HttpRequest
-from rest_framework.decorators import api_view, authentication_classes
+from rest_framework.decorators import api_view, authentication_classes, permission_classes
+from authentication.permissions import ValidateBasket
 
-
-@api_view(["GET", 'POST'])
+@api_view(["GET", "POST"])
 @authentication_classes([JWTAuthentication])
+@permission_classes([ValidateBasket])
 def payment_gateway(request: HttpRequest):
     basket = BasketAndOrderRedisAdapter(request)
     if request.method == "POST":
@@ -15,10 +16,11 @@ def payment_gateway(request: HttpRequest):
         expiration_date = request.POST.get("exp_date")
         password = request.POST.get("password")
         for item in [card_number, cvv2, expiration_date, password]:
-            if item == None:
+            if item is None:
                 basket.set_payment_information(message="fail")
                 return Response({"message": "please enter all information"})
             basket.set_payment_information(message="success")
+            basket.create_order()
             return Response({"message": "payment was successful!"})
     else:
         return Response({"message": f"pay {basket.total_price}"})
